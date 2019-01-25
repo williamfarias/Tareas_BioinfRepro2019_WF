697c76ed7262cd546932bcaef888eed007bb73de
williamfarias <william.farias@ug.uchile.cl>
2019-01-25 14:34:06 -0300

    cambio de CG a GC en grep

--- tarea_1_3.sh
+++ tarea_1_3.sh
@@ -3,5 +3,5 @@
 mkdir seqpurpuratus
 #bajar 5 secuencias de NCBI en directorio como archivo purpuratus
 curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&rettype=fasta&id=971888982,341865493,365176258,3651766257,365176253" > seqpurpuratus/purpuratus.fasta
-#buacar cantidad de veces que aparece la secuencia TCGA
-grep -c "TCGA" seqpurpuratus/purpuratus.fasta
+#buacar cantidad de veces que aparece la secuencia TGCA
+grep -c "TGCA" seqpurpuratus/purpuratus.fasta
